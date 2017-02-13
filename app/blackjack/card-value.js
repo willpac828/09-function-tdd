e1708594aee30516a10d61ae57577046b77f59c4
Will Pacetti <Slade@Will-Pacettis-MacBook-Pro.local>
2017-02-13 15:59:58 -0600

    Established card values

--- app/blackjack/card-value.js
+++ app/blackjack/card-value.js
@@ -1,1 +1,25 @@
-export default function () {}
+export default function (cardValue, card1, card2) {
+  // for (i = 2; i < 11; i += 1) {
+  //   if
+  // }
+  //
+
+  if (cardValue === 'jack') {
+    return 10;
+  }
+
+  if (cardValue === 'queen') {
+    return 10;
+  }
+
+  if (cardValue === 'king') {
+    return 10;
+  }
+
+  if (cardValue === 'ace') {
+    return 11;
+  }
+
+  return parseInt(cardValue);
+
+}
